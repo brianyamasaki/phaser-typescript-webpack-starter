92063339af71503f380bdb4bc0eb5ab02c9456ed
Brian Yamasaki <brian.yamasaki@SeattleGT.com>
2016-12-15 14:03:28 -0800

    Simplify Start and End

--- src/js/states/gameOver.ts
+++ src/js/states/gameOver.ts
@@ -3,6 +3,8 @@
 import { RainbowText } from '../objects/RainbowText';
 
 export class GameOver extends Phaser.State {
+	rainbowText: RainbowText;
+	text: Phaser.Text;
 
 	init() {
 
@@ -14,12 +16,22 @@ export class GameOver extends Phaser.State {
 
 	create() {
 		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
-		let text = new RainbowText(this.game, center.x, center.y, "Game Over");
-		text.anchor.set(0.5);
+		let style = { font: "bold 14px Arial", fill: "#fff", align: 'center'};
+		this.rainbowText = new RainbowText(this.game, center.x, center.y, "Game Over");
+		this.rainbowText.anchor.set(0.5);
+		this.text = this.game.add.text(center.x - 60, center.y + 50, 'Click to Restart', style);
+	}
+
+	cleanup() {
+		this.rainbowText.kill();
 	}
 
 	update() {
-		
+		if (this.game.input.mousePointer.isDown) {
+			this.cleanup();
+			this.game.state.start('GameStart');
+		}
+
 	}
 
 }
--- src/js/states/gameStart.ts
+++ src/js/states/gameStart.ts
@@ -3,7 +3,8 @@
 import { RainbowText } from '../objects/RainbowText';
 
 export class GameStart extends Phaser.State {
-	text:RainbowText;
+	rainbowText:RainbowText;
+	text:Phaser.Text;
 
 	init() {
 
@@ -15,18 +16,21 @@ export class GameStart extends Phaser.State {
 
 	create() {
 		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
-		this.text = new RainbowText(this.game, center.x, center.y, "Phaser TypeScript\n Click to continue");
-		this.text.anchor.set(0.5);		
+		let style = { font: "bold 14px Arial", fill: "#fff", align: 'center'};
+		this.rainbowText = new RainbowText(this.game, center.x, center.y, "Phaser TypeScript");
+		this.rainbowText.anchor.set(0.5);		
+		this.text = this.game.add.text(center.x - 60, center.y + 50, 'Click to Continue', style);
 	}
 
-	exitState() {
+	cleanup() {
+		this.rainbowText.kill();
 		this.text.kill();
-		this.game.state.start('GameOver');
 	}
 
 	update() {
 		if (this.game.input.mousePointer.isDown) {
-			this.exitState();
+			this.cleanup();
+			this.game.state.start('GameOver');
 		}
 	}
 
